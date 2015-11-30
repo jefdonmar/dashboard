cc3b6ccadcc223921aa42a707f96e87a7517b4ad
Jeffrey Martín <jefdonmar@gmail.com>
2015-11-30 13:15:34 -0500

    Login Auth Token Success with heroku

--- js/app-user/controllers/login.controller.js
+++ js/app-user/controllers/login.controller.js
@@ -2,6 +2,21 @@ let LoginController = function($scope, $state, UserService) {
   
   console.log('Hello from the LoginController');
 
+  // set view model to this object
+  let vm = this;
+
+  // set of functions we will define in the controller
+  vm.login = login;
+
+  function login (userObj) {
+    console.log(userObj);
+    UserService.login(userObj).then( (res) => {
+      let loginRES = res.data.user;
+      console.log(loginRES);
+      UserService.storeAuth(loginRES);
+    });
+  }
+
 };
 
 LoginController.$inject = ['$scope', '$state', 'UserService'];
