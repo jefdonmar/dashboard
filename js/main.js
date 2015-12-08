f5d8d0b3fcdf6542252bcb0b3fe6a3b03343de47
Jeffrey Martín <jefdonmar@gmail.com>
2015-12-07 23:45:54 -0500

    had to fix something that happened in our app main.js that i did see in the merge conflicts

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-=======
 // Import Chart JS - move to another file later
 import Chart from 'chart.js';
 console.dir(Chart);
-
 
 import $ from 'jquery';
 import 'foundation';
@@ -11,7 +8,7 @@ import 'foundation';
 //Initialize Foundation
 $(document).foundation();
 
->>>>>>> jeff
+
 // Import angular
 import angular from 'angular';
 
